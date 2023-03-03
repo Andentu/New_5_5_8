25c2b98be18dfc3024603aceaacaf54ca944fc54
Andentu <severalmaz97@gmail.com>
2023-03-03 16:39:47 +0300

    Добавлен код для рассчетра результатат введенного числа в определенной степени

--- New_5_5_8/Program.cs
+++ New_5_5_8/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\nВведите степень, в которую нужно возвести число: ");
+            byte power = byte.Parse(Console.ReadLine());
+            PowerUp(num, power);
+            Console.Write("\nРезультат: " + PowerUp(num, power));
+
+            Console.ReadKey();
+        }
+        private static decimal PowerUp(decimal N, byte pow)
+        {
+            // decimal result;
+            if (pow == 0)
+            {
+                return 1;
+            }
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow); // N * на N столько раз, сколько указано в pow
+            }
+
         }
     }
 }
